bbe11a81f3018090372222f45faca2e6c912fa52
Deepanjay Nandal <75788118+DeepanjayNandal@users.noreply.github.com>
2024-11-04 18:59:36 -0700

    Create Solution1.java

---
+++ 242. Valid Anagram/Solution1.java
@@ -0,0 +1,14 @@
+// Time complexity = 0(nlogn)
+// Space complexity = 0(1)
+
+class Solution1 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars, tChars);
+    }
+}
