4d0b9a52254ea2646092b88a5ee4f5ec6502ff94
Deepanjay Nandal <75788118+DeepanjayNandal@users.noreply.github.com>
2025-01-11 04:23:42 -0700

    Create OptimalTwoPointerSolution.java

---
+++ 15. 3Sum/OptimalTwoPointerSolution.java
@@ -0,0 +1,33 @@
+// Time : 0(N^2)
+// Space : 0(1) (in hashmap it was 0(N) hence OPTIMAL
+
+class OptimalTwoPointerSolution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                }
+            }
+        }
+        return result;
+    }
+}
