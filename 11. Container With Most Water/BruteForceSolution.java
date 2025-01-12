fbacc8233efa688aa40cc1791a00ab0204a58d60
Deepanjay Nandal <75788118+DeepanjayNandal@users.noreply.github.com>
2025-01-12 05:18:08 -0700

    Create BruteForceSolution.java

---
+++ 11. Container With Most Water/BruteForceSolution.java
@@ -0,0 +1,15 @@
+public class BruteForceSolution {
+    public int maxArea(int[] height) {
+        int maxarea = 0;
+        for (int left = 0; left < height.length; left++) {
+            for (int right = left + 1; right < height.length; right++) {
+                int width = right - left;
+                maxarea = Math.max(
+                    maxarea,
+                    Math.min(height[left], height[right]) * width
+                );
+            }
+        }
+        return maxarea;
+    }
+}
